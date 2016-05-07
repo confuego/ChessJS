ee5f9f7f4f12b7f4797764e1d9bc787a77afcb23
Wesley Chappell <wes.chappell10@gmail.com>
2016-05-06 21:10:22 -0400

    Board loads from socket event now

--- http/index.js
+++ http/index.js
@@ -1,24 +1,25 @@
-function CreateBoard(div) {
-	var table = document.createElement("table");
-	table.id = "chess_board";
-
-	for(var i = 0; i < 8; i++) {
-
-		var row = document.createElement("tr");
-
-		for(var j = 0; j < 8; j++) {
-			var cell = document.createElement("td");
-			cell.id = i.toString() + "-" + j.toString();
-			row.appendChild(cell);
-		}
-		table.appendChild(row);
-
-	}
-
-	document.getElementById("chess").appendChild(table);
-}
 
 window.onload = function() {
+
+	function CreateBoard(div) {
+		var table = document.createElement("table");
+		table.id = "chess_board";
+
+		for(var i = 0; i < 8; i++) {
+
+			var row = document.createElement("tr");
+
+			for(var j = 0; j < 8; j++) {
+				var cell = document.createElement("td");
+				cell.id = i.toString() + "-" + j.toString();
+				row.appendChild(cell);
+			}
+			table.appendChild(row);
+
+		}
+
+		document.getElementById(div).appendChild(table);
+	}
 
 	var socket = io.connect("http://10.0.226.89:8080", { resource: "nodejs" });
 	var CurrentRoom = null;
@@ -119,7 +120,7 @@ window.onload = function() {
 	});
 
 	socket.on("Initialize Board", function(BoardStruct) {
-		
+		CreateBoard("chess");
 	});
 };
 
